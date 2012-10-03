838772729de44aa30c7dccdaec6fbe3a698e5784
Shannon <shannon_c_wallace@yahoo.com>
2012-10-03 09:50:27 -0400

    first commit for OAuth2

---
+++ Debug Form/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+
+using System.Text;
+using System.Windows.Forms;
+using ConstantContactBO;
+using ConstantContactUtility;
+
+
+namespace Debug_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DEBUG TESTING
+            AuthenticationData authdata = new AuthenticationData();  //Set your credentials for basic or OAuth2 inside the parentheses
+          
+            //Your test code below
+        }
+    }
+}
